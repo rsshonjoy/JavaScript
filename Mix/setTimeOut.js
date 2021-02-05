c8c130b16af13e2e662a6e8d620996222abc4543
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-05 19:34:39 +0600

    setTimeOut and setInterval

---
+++ Mix/setTimeOut.js
@@ -0,0 +1,62 @@
+/**----- Step 1 -----*/
+
+// console.log(2222);
+// console.log(3333);
+// console.log(4444);
+
+
+/**----- Step 2 -----*/
+
+// function doSomeThing() {
+//     console.log(3333);
+// }
+// console.log(2222);
+// doSomeThing()
+// console.log(4444);
+
+
+/**----- Step 3 -----*/
+
+// function doSomeThing() {
+//     console.log(3333);
+// }
+// console.log(2222);
+// setTimeout(doSomeThing)
+// console.log(4444);
+
+
+/**----- Step 4 -----*/
+
+// function doSomeThing() {
+//     console.log(3333);
+// }
+// console.log(2222);
+// setTimeout(doSomeThing, 2000)
+// console.log(4444);
+
+
+/**----- Step 5 -----*/
+
+// console.log(2222);
+// setTimeout(function() {
+//     console.log('lazy and waiting');
+// }, 4000)
+// console.log(4444);
+
+
+/**----- Step 6 -----*/
+
+// console.log(2222);
+// setTimeout(() => {
+//     console.log('see you later.');
+// }, 4000)
+// console.log(4444);
+
+
+/**----- Step 6 -----*/
+
+setInterval(function () {
+    console.log('doing it');
+}, 1000)
+console.log(2222);
+console.log(4444);
