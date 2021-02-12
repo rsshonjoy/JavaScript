f561c67031e0fdcca286c714e29af71acd861b53
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-12 21:34:43 +0600

    object

---
+++ Class/class.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(sId, sName){
+        this.id = sId;
+        this.name = sName;
+        this.school = "x school"
+    }
+}
+
+const student1 = new Student(12, "Shouvo")
+const student2 = new Student(13, "Joy")
+const student3 = new Student(14, "Raj")
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
