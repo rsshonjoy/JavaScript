6efe0ba4214ae18d50007ab057779614e4a27dc3
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-15 23:36:52 +0600

    ternary operator

--- ternary-operator.js
+++ ternary-operator.js
@@ -15,3 +15,22 @@
 const money = 150;
 let food = money > 100 ? 'birayni' : 'alur vorta';
 console.log(food);
+
+
+
+const active = false;
+
+const x = active && 5;
+const y = active || 5;
+
+console.log(x);
+console.log(y);
+
+
+// String to Number
+const number = + '37'
+console.log(number);
+
+// number to String
+const numText = 37 + '';
+console.log(numText);
