05bdd3d711f38c26c75f6dc16592d98c1fc674f2
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-16 11:11:42 +0600

    object shortcut

--- shortcuts.js
+++ shortcuts.js
@@ -1,8 +1,33 @@
+/** ----- Conversion ----- */
+
 
 // String to Number
-const number = + '37'
-console.log(number);
+
+// const number = + '37'
+// console.log(number);
+
 
 // number to String
-const numText = 37 + '';
-console.log(numText);
+
+// const numText = 37 + '';
+// console.log(numText);
+
+
+// create object shortcut
+
+// method 1
+
+// const x = 25;
+// const y = 25;
+
+// const obj = {x: x, y:y};
+// console.log(obj);
+
+
+// method 2
+
+const x = 25;
+const y = 25;
+const obj = {x, y};
+
+console.log(obj);
