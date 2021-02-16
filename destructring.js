b343ec3d3ec0c306a9c8d4f0d4360eecca4873e0
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-16 10:22:29 +0600

    destructring step 2

--- destructring.js
+++ destructring.js
@@ -1,6 +1,12 @@
 // Step 1
 
-const numbers = [18, 19];
-const x = numbers[0];
-const y = numbers[1];
+// const numbers = [18, 19];
+// const x = numbers[0];
+// const y = numbers[1];
+// console.log(x, y);
+
+
+// Step 2
+
+const [x, y] =  [18, 19];
 console.log(x, y);
