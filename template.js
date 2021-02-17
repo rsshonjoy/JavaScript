ffd6de2e8131a655c5e0c0447102510708cc3823
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-02-17 23:48:50 +0600

    template

---
+++ template.js
@@ -0,0 +1,19 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+console.log(fullName);
+
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+console.log(fullName2);
+
+const multiLine = "I love you\n"
+    + "I miss you\n"
+    + "I need you";
+console.log(multiLine);
+
+const multiLine2 = `I love you
+I miss you
+no. I don't need you
+Baily road e dorkar nai.`
+
+console.log(multiLine2);
