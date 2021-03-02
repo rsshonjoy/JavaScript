27e5bf0a3db1385b96e826302946b576b52a7dce
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-02 18:14:54 +0600

    reduce

--- reduce.js
+++ reduce.js
@@ -3,7 +3,9 @@ const nums = [12, 45, 42, 19, 27,37];
 const map = nums.map(num => num* 2);
 const filter = nums.filter(num => num > 20);
 const find = nums.find(num => num > 20);
+const reduce = nums.reduce((sum, num) => sum + num, 0);
 
 console.log("map: ", map);
 console.log("filter: ", filter);
 console.log("find: ", find);
+console.log("reduce: ", reduce);
