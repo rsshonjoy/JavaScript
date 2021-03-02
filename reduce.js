8bb8ce9ad3f51acde78029db14d4c42734f17e5d
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-02 18:31:04 +0600

    object in reduce

--- reduce.js
+++ reduce.js
@@ -1,3 +1,5 @@
+// array in reduce
+
 const nums = [12, 45, 42, 19, 27,37];
 
 const map = nums.map(num => num* 2);
@@ -13,3 +15,19 @@ console.log("map: ", map);
 console.log("filter: ", filter);
 console.log("find: ", find);
 console.log("reduce: ", reduce);
+
+
+
+//  object in reduce
+
+const persons = [
+  {name: 'John', age:37,},
+  {name: 'Jacika', age:18,},
+  {name: 'Rose', age:19,}
+];
+
+const personInfo = persons.reduce((sum, person) => {
+  console.log(sum, person);
+  return sum + person.age;
+}, 0);
+console.log("personInfo: ", personInfo);
