57633981d31d9ea9834044ce3066e85408399a71
Shonjoy Das <rsshonjoydas371819@gmail.com>
2021-03-02 18:06:57 +0600

    map, filter, find

---
+++ reduce.js
@@ -0,0 +1,9 @@
+const nums = [12, 45, 42, 19, 27,37];
+
+const map = nums.map(num => num* 2);
+const filter = nums.filter(num => num > 20);
+const find = nums.find(num => num > 20);
+
+console.log("map: ", map);
+console.log("filter: ", filter);
+console.log("find: ", find);
